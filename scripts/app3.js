8e6841a87accf626677c2e7f1d68a83fd21a99d2
jackcolin21307 <jackcolin21307@gmail.com>
2022-07-26 17:45:21 -0300

    Complete the Challenge 3

--- scripts/app3.js
+++ scripts/app3.js
@@ -44,9 +44,3 @@ botaoLimpar.addEventListener('click', function () {
     limparSaldo();
     soma.value = "";
 });
-/**
-    <h4>Valor a ser adicionado: <input id="soma"> </h4>
-    <button id="atualizar-saldo">Atualizar saldo</button>
-    <button id="limpar-saldo">Limpar seu saldo</button>
-    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
- */ 
